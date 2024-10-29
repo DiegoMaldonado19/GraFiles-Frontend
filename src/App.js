b2eb06e3adee25ef669fbb8d107f5983a5d913c5
Diego Maldonado <dj_maldonado19@hotmail.es>
2024-10-29 12:14:58 -0600

    Adding changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './views/Login';
+import ViewAdministrador from './views/ViewAdministrador';
+import ViewEmpleado from './views/ViewEmpleado';
+import TextEditorPage from './components/TextEditorPage';
+import DirectorySelect from './views/DirectorySelect';
+import NewDocument from './components/NewDocument';
+import Register from './views/Register';
+import SharedFiles from './views/SharedFiles';
+import PaperBin from './views/PaperBin';
+import TextEditorReadPage from './components/TextEditorReadPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/admin" element={<ViewAdministrador />} />
+        <Route path="/empleado" element={<ViewEmpleado />} />
+        <Route path="/editor/:fileId/:userId/:filename" element={<TextEditorPage />} />
+        <Route path="/directory/:directoryId/:directoryName" element={<DirectorySelect />} />
+        <Route path="/editor/:directoryId/nuevo-documento" element={<NewDocument />} />
+        <Route path="/sharedirectory" element={<SharedFiles />} />
+        <Route path="/paperbin" element={<PaperBin />} />
+        <Route path="/editorRead/:fileId/:userId/:filename" element={<TextEditorReadPage />} />
+      </Routes>
+    </Router>
   );
 }
 
