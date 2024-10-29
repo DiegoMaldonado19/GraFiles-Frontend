772ca7654d35834e1b04cc453ca9d27cee422e82
Diego Maldonado <dj_maldonado19@hotmail.es>
2024-10-29 06:59:13 -0600

    Adding assets and changes on login view

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import Swal from 'sweetalert2';
-import logo from "../assets/1.png";
 const Login = () => {
   const [formData, setFormData] = useState({
     identifier: '',
@@ -58,7 +57,6 @@ const Login = () => {
     <section className="bg-white white:bg-dark-900">
       <div className="flex justify-center min-h-screen">
         <div className=" w-2/5 grid justify-items-center  place-content-center bg-gray-100" >
-        <img src={logo} alt="Imagen de fondo" width={400} height={400}  />
         </div>
 
         <div className="flex items-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5">
